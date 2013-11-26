35ea0b156274d76302f9bf93abdd4197325cad37
zhouqi <bitwolaiye@gmail.com>
2013-11-26 23:26:19 +0800

    add django rest framework to project, but the framework has changed a lot, so I need some time to dive into it.

--- pet_api/pet/models.py
+++ pet_api/pet/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, db_column='user_name')
     password = models.CharField(max_length=100)
     email = models.CharField(max_length=50, null=True)
     gender = models.PositiveSmallIntegerField(default=0)
